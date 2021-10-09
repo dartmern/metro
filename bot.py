320d62296e269a193e4f07c5d5dcd8cc1cf1b5cf
dartmern <eric.ni.cheng@gmail.com>
2021-10-08 22:56:00 -0700

    added `get_or_fetch_member` (bot method)

--- bot.py
+++ bot.py
@@ -216,6 +216,38 @@ class MetroBot(commands.AutoShardedBot):
 
         await self.invoke(ctx)
 
+    async def get_or_fetch_member(self, guild, member_id):
+        """Looks up a member in cache or fetches if not found.
+        Parameters
+        -----------
+        guild: Guild
+            The guild to look in.
+        member_id: int
+            The member ID to search for.
+        Returns
+        ---------
+        Optional[Member]
+            The member or None if not found.
+        """
+
+        member = guild.get_member(member_id)
+        if member is not None:
+            return member
+
+        shard = self.get_shard(guild.shard_id)
+        if shard.is_ws_ratelimited():
+            try:
+                member = await guild.fetch_member(member_id)
+            except discord.HTTPException:
+                return None
+            else:
+                return member
+
+        members = await guild.query_members(limit=1, user_ids=[member_id], cache=True)
+        if not members:
+            return None
+        return members[0]
+
 
 
 intents = discord.Intents.all()
