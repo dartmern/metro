75fcf9a06869bf1c20f7ef157c9c74762e3e3320
dartmern <eric.ni.cheng@gmail.com>
2022-01-19 15:27:48 -0800

    blacklist table

--- database/blacklist.sql
+++ database/blacklist.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS blacklist
 (
     member_id bigint NOT NULL,
     is_blacklisted boolean NOT NULL,
+    moderator bigint NOT NULL,
+    added_time timestamp without time zone NOT NULL,
     reason text COLLATE pg_catalog."default",
     CONSTRAINT is_blacklisted UNIQUE (is_blacklisted, member_id),
     CONSTRAINT member_id UNIQUE (member_id, is_blacklisted)
