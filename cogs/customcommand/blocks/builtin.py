cf511235e067e38d13537571ffa213a4698bf8d1
dartmern <eric.ni.cheng@gmail.com>
2022-02-12 22:21:23 -0800

    add command blocks from tse

---
+++ cogs/customcommand/blocks/builtin.py
@@ -0,0 +1,30 @@
+import TagScriptEngine as tse
+from .delete import DeleteBlock
+
+blocks = [
+            tse.MathBlock(),
+            tse.RandomBlock(),
+            tse.RangeBlock(),
+            tse.AnyBlock(),
+            tse.IfBlock(),
+            tse.AllBlock(),
+            tse.BreakBlock(),
+            tse.StrfBlock(),
+            tse.StopBlock(),
+            tse.AssignmentBlock(),
+            tse.FiftyFiftyBlock(),
+            tse.ShortCutRedirectBlock("args"),
+            tse.LooseVariableGetterBlock(),
+            tse.SubstringBlock(),
+            tse.EmbedBlock(),
+            tse.ReplaceBlock(),
+            tse.PythonBlock(),
+            tse.URLEncodeBlock(),
+            tse.RequireBlock(),
+            tse.BlacklistBlock(),
+            tse.CommandBlock(),
+            tse.OverrideBlock(),
+            tse.RedirectBlock(),
+            tse.CooldownBlock(),
+            DeleteBlock(),
+]
