85c52cd5f35819d1746b80e441e1ffaca22befef
dartmern <eric.ni.cheng@gmail.com>
2022-04-30 22:50:34 -0700

    Start to make a stats command

---
+++ cogs/stats.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+from bot import MetroBot
+
+async def setup(bot: MetroBot):
+    await bot.add_cog(stats(bot))
+
+class stats(commands.Cog, description='Bot statistics tracking related.'):
+    def __init__(self, bot: MetroBot):
+        self.bot = bot
+
+    @property
+    def emoji(self) -> str:
+        return '\U0001f4c8'
+
+
